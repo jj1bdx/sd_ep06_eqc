a35adae9a52945545b7651409c7ee3d2e1f85e5d
Kenji Rikitake <kenji.rikitake@acm.org>
2015-07-13 16:09:25 +0900

    Add tests to sd_eqc_app.erl

--- src/sd_eqc_app.erl
+++ src/sd_eqc_app.erl
@@ -27,4 +27,30 @@ simple_test() ->
     ok = application:start(sd_eqc),
     ?assertNot(undefined == whereis(sd_eqc_sup)).
 
+counter_test_() ->
+    {setup,
+     fun counter_start_link/0,
+     fun counter_stop/1,
+     fun counter_check/1}.
+
+counter_start_link() ->
+    {ok, Pid} = msgcounter_gen_server:start_link(),
+    Pid.
+
+counter_stop(Pid) ->
+    msgcounter_gen_server:stop(Pid).
+
+counter_check(Pid) ->
+    [?_assertEqual(0, msgcounter_gen_server:val(Pid)),
+    ?_assertEqual(1, msgcounter_gen_server:inc(Pid)),
+    ?_assertEqual(2, msgcounter_gen_server:inc(Pid)),
+    ?_assertEqual(ok, msgcounter_gen_server:zero(Pid)),
+    ?_assertEqual(0, msgcounter_gen_server:val(Pid)),
+    ?_assertEqual(1, msgcounter_gen_server:inc(Pid)),
+    ?_assertEqual(0, msgcounter_gen_server:dec(Pid)),
+    ?_assertEqual(-1, msgcounter_gen_server:dec(Pid)),
+    ?_assertEqual(0, msgcounter_gen_server:inc(Pid)),
+    ?_assertEqual(0, msgcounter_gen_server:val(Pid))
+    ].
+
 -endif.
